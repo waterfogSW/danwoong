75eaa57c4d7432ce4826f332ec709cf9ea5f9fd0
San Kim <waterfog9580@gmail.com>
2022-06-06 01:16:48 +0900

    :sparkles: Unknown 메시지 타입 추가

--- src/main/java/com/dku/danwoong/message/model/MessageType.java
+++ src/main/java/com/dku/danwoong/message/model/MessageType.java
@@ -3,7 +3,8 @@ package com.dku.danwoong.message.model;
 public enum MessageType {
     WELCOME,
     SAVE_RECORD,
-    LOOKUP_RECORD;
+    LOOKUP_RECORD,
+    UNKNOWN_INTENT;
 
     public static MessageType from(String intentType) {
         return MessageType.valueOf(intentType.toUpperCase());
