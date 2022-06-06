e7b5c7e18dd6a967915e47cfa66a72f6191b3081
San Kim <waterfog9580@gmail.com>
2022-06-06 23:57:30 +0900

    :adhesive_bandage: 인자 validation 로직 추가

--- src/main/java/com/dku/danwoong/record/service/DefaultRecordService.java
+++ src/main/java/com/dku/danwoong/record/service/DefaultRecordService.java
@@ -3,6 +3,7 @@ package com.dku.danwoong.record.service;
 import com.dku.danwoong.record.entity.Record;
 import com.dku.danwoong.record.repository.RecordRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class DefaultRecordService implements RecordService {
@@ -15,6 +16,7 @@ public class DefaultRecordService implements RecordService {
 
     @Override
     public void save(Record record) {
+        Assert.notNull(record, "Record must be provided");
         recordRepository.save(record);
     }
 }
